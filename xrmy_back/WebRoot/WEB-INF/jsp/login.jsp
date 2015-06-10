afc779858de23e814d26490d7aefe22038d022ed
guokaige <kaige8312@163.com>
2015-06-10 17:24:13 +0800

    添加user

--- xrmy_back/WebRoot/WEB-INF/jsp/login.jsp
+++ xrmy_back/WebRoot/WEB-INF/jsp/login.jsp
@@ -8,8 +8,8 @@
 </head>
 <body>
 <form action="${pageContext.request.contextPath }/login.action" method="post">
-用户账号：<input type="text" name="username" /><br/>
-用户密码 ：<input type="password" name="password" /><br/>
+用户账号：<input type="text" name="userCustom.username" /><br/>
+用户密码 ：<input type="password" name="userCustom.password" /><br/>
 <input type="submit" value="登陆"/>
 </form>
 </body>
--- xrmy_back/src/cn/xrmy/controller/LoginController.java
+++ xrmy_back/src/cn/xrmy/controller/LoginController.java
@@ -2,24 +2,36 @@
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.xrmy.biz.NewsService;
+import cn.xrmy.biz.UserService;
+import cn.xrmy.pojo.UserCustom;
+import cn.xrmy.pojo.UserQueryVo;
+
 @Controller
 public class LoginController {
-
+	@Autowired
+	private UserService userService;
 	// 登陆
 	@RequestMapping("/login")
-	public String login(HttpSession session, String username, String password)
+	public String login(HttpSession session, UserQueryVo userQueryVo)
 			throws Exception {
 
 		// 调用service进行用户身份验证
 		// ...
-
-		// 在session中保存用户身份信息
-		session.setAttribute("username", username);
-		// 重定向到商品列表页面
-		return "redirect:/news/queryNews.action";
+		UserCustom  userCustom=userService.findNewsByNameAndPwd(userQueryVo);
+       if(null!=userCustom){
+    	// 在session中保存用户身份信息
+   		 session.setAttribute("username", userQueryVo.getUserCustom().getUsername());
+   		// 重定向到商品列表页面
+   		   return "redirect:/news/queryNews.action";
+       }else{
+    	   return "redirect:/news/queryNews.action";
+       }
+		
 	}
 
 	// 退出
